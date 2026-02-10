cfbeabdb5a64a56c0867486537833f7c35e47a05
FrizakN1 <frizak2017@mail.ru>
2026-02-10 16:59:52 +0500

    Добавил разрешение на загрузку файлов .cdr

--- frontend/src/components/UploadFile.jsx
+++ frontend/src/components/UploadFile.jsx
@@ -60,80 +60,81 @@ const UploadFile = ({returnFile, type, onlyImage = false}) => {
     const handlerFileChange = (event) => {
         const file = event.target.files[0];
 
-        if (file) {
-            const fileMimeType = mime.lookup(file.name)
-            const allowedMimeTypes = onlyImage ?
-                [
-                    "image/png",
-                    "image/jpeg"
-                ]
+        if (!file) {return}
+
+        const fileMimeType = mime.lookup(file.name)
+        const allowedMimeTypes = onlyImage ?
+            [
+                "image/png",
+                "image/jpeg"
+            ]
             :
-                [
-                    "application/msword",
-                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                    "application/pdf",
-                    "text/plain",
-                    "application/vnd.ms-excel",
-                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                    "application/x-rar-compressed",
-                    "application/zip",
-                    "application/x-7z-compressed",
-                    "application/x-tar",
-                    "application/vnd.rar",
-                    "image/png",
-                    "image/jpeg"
-                ]
-            if (allowedMimeTypes.includes(fileMimeType)) {
-                if (file.size <= 10485760) {
-                    const reader = new FileReader();
-
-                    reader.onload = (e) => {
-                        let icon
-
-                        switch (fileMimeType) {
-                            case "application/msword":
-                            case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-                                icon = <FontAwesomeIcon icon={faFileWord} />; break
-                            case "application/pdf": icon = <FontAwesomeIcon icon={faFilePdf} />; break
-                            case "text/plain": icon = <FontAwesomeIcon icon={faFileLines} />; break
-                            case "application/vnd.ms-excel":
-                            case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-                                icon = <FontAwesomeIcon icon={faFileExcel} />; break
-                            case "application/x-rar-compressed":
-                            case "application/zip":
-                            case "application/x-7z-compressed":
-                            case "application/x-tar":
-                            case "application/vnd.rar":
-                                icon = <FontAwesomeIcon icon={faFileZipper} />; break
-                            case "image/png":
-                            case "image/jpeg":
-                                icon = <FontAwesomeIcon icon={faFileImage} />; break
-                            default:
-                                icon = <FontAwesomeIcon icon={faFile} />
-                        }
-
-                        setUploadFile({
-                            File: file,
-                            Name: file.name,
-                            Icon: icon,
-                        })
-                    }
-
-                    reader.readAsDataURL(file)
-                } else {
-                    alert("Размер файла слишком большой.");
-                }
-            } else {
-                alert("Недопустимый формат файла. Пожалуйста, выберите файл другого формата.");
-            }
+            [
+                "application/msword",
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                "application/pdf",
+                "text/plain",
+                "application/vnd.ms-excel",
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "application/x-rar-compressed",
+                "application/zip",
+                "application/x-7z-compressed",
+                "application/x-tar",
+                "application/vnd.rar",
+                "image/png",
+                "image/jpeg",
+                "application/octet-stream",
+            ]
+        if (!allowedMimeTypes.includes(fileMimeType)) {
+            alert("Недопустимый формат файла. Пожалуйста, выберите файл другого формата.");
         }
+
+        if (file.size > 10485760) {
+            alert("Размер файла слишком большой.");
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            let icon
+
+            switch (fileMimeType) {
+                case "application/msword":
+                case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                    icon = <FontAwesomeIcon icon={faFileWord} />; break
+                case "application/pdf": icon = <FontAwesomeIcon icon={faFilePdf} />; break
+                case "text/plain": icon = <FontAwesomeIcon icon={faFileLines} />; break
+                case "application/vnd.ms-excel":
+                case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                    icon = <FontAwesomeIcon icon={faFileExcel} />; break
+                case "application/x-rar-compressed":
+                case "application/zip":
+                case "application/x-7z-compressed":
+                case "application/x-tar":
+                case "application/vnd.rar":
+                    icon = <FontAwesomeIcon icon={faFileZipper} />; break
+                case "image/png":
+                case "image/jpeg":
+                    icon = <FontAwesomeIcon icon={faFileImage} />; break
+                default:
+                    icon = <FontAwesomeIcon icon={faFile} />
+            }
+
+            setUploadFile({
+                File: file,
+                Name: file.name,
+                Icon: icon,
+            })
+        }
+
+        reader.readAsDataURL(file)
     }
 
     return (
         <div className="contain">
             <div className="upload">
                 <label>
-                    <input type="file" ref={fileInputRef} accept={onlyImage ? ".png, .jpeg, .jpg" : ".doc, .docx, .pdf, .txt, .xls, .xlsx, .rar, .zip, .7z, .tar, .png, .jpeg, .jpg"} max="10485760"
+                    <input type="file" ref={fileInputRef} accept={onlyImage ? ".png, .jpeg, .jpg" : ".doc, .docx, .pdf, .txt, .xls, .xlsx, .rar, .zip, .7z, .tar, .png, .jpeg, .jpg .cdr"} max="10485760"
                     onInput={handlerFileChange}/>
                     {uploadFile.File != null ?
                         <div>{uploadFile.Icon} <span>{uploadFile.Name}</span></div>
